27c6e194f6337758e4a29a39363019d557a2fd35
namYeJi9q <nyj5850@naver.com>
2024-06-04 12:41:03 +0900

    [fix] dashboard DB 이름 표시 추가

--- lib/pages/webtoon_app/screens/detail_screen.dart
+++ lib/pages/webtoon_app/screens/detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:run_flutter_ui/services/api_service.dart';
+
+import '../../../models/webtoon_detail_model.dart';
 
 class DetailScreen extends StatelessWidget {
   final String title, thumb, id;
@@ -12,42 +15,57 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 2,
-          backgroundColor: const Color(0xfff2b33a),
-          foregroundColor: Colors.black,
-          title: Text(
-            title,
-            style: const TextStyle(fontSize: 20),
-          ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: const Color(0xfff2b33a),
+        foregroundColor: Colors.black,
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 20),
         ),
-        body: Column(
-          children: [
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      body: FutureBuilder<WebtoonDetailModel>(
+        future: ApiService.getToonById(id),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text('No Data'));
+          } else {
+            final webtoonDetail = snapshot.data!;
+            return Column(
               children: [
-                Hero(
-                  tag: id,
-                  child: Container(
-                    width: 250,
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 12,
-                            offset: const Offset(10, 10),
-                            color: Colors.black.withOpacity(0.3),
-                          ),
-                        ]),
-                    child: Image.network(thumb),
-                  ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: id,
+                      child: Container(
+                        width: 250,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 12,
+                                offset: const Offset(10, 10),
+                                color: Colors.black.withOpacity(0.3),
+                              ),
+                            ]),
+                        child: Image.network(thumb),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            ),
-          ],
-        ));
+            );
+          }
+        },
+      ),
+    );
   }
 }
